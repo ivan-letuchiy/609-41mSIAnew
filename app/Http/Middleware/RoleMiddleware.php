0e7f150d8aaf9ed1823176a7ef9245fbafbbd6d0
ivan-letuchiy <letuchiyecosystem@gmail.com>
2025-09-25 14:51:21 +0500

    Установка Sanctum

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class RoleMiddleware
@@ -10,12 +11,12 @@ class RoleMiddleware
     /**
      * Handle an incoming request.
      *
-     * @param \Illuminate\Http\Request $request
-     * @param  \Closure  $next
+     * @param Request $request
+     * @param Closure  $next
      * @param string $role
      * @return mixed
      */
-    public function handle(\Illuminate\Http\Request $request, Closure $next, string $role): mixed
+    public function handle(Request $request, Closure $next, string $role): mixed
     {
         // Проверяем, авторизован ли пользователь и соответствует ли его роль требуемой
         if (!Auth::check() || Auth::user()->role !== $role) {
--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class, 'login']);
 
-
-Route::get('/users', [\App\Http\Controllers\UsersControllerApi::class, 'index']);
-Route::get('/users/{user}', [\App\Http\Controllers\UsersControllerApi::class, 'show']);
-Route::get("/houses", [\App\Http\Controllers\HousesControllerApi::class, 'index']);
-Route::get("/houses/{house}", [\App\Http\Controllers\HousesControllerApi::class, 'show']);
-Route::get('/flats', [\App\Http\Controllers\FlatsControllerApi::class, 'index']);
-Route::get('/flats/{flat}', [\App\Http\Controllers\FlatsControllerApi::class, 'show']);
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/houses', [\App\Http\Controllers\HousesControllerApi::class, 'index']);
+    Route::get('/houses/{house}', [\App\Http\Controllers\HousesControllerApi::class, 'show']);
+    Route::get('/flats', [\App\Http\Controllers\FlatsControllerApi::class, 'index']);
+    Route::get('/flats/{flat}', [\App\Http\Controllers\FlatsControllerApi::class, 'show']);
+    Route::get('/users', function (Request $request) {return $request->user();});
+    Route::get('/logout', [AuthController::class, 'logout']);
+});
